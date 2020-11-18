2b17ae193ad2f14dafe968fc80b16a37743747b6
DaniHoo <daniel.hoogen@web.de>
2020-11-18 19:16:16 +0100

    basic solution for MaterialSpinner

--- MaterialComponents/src/main/java/de/danihoo94/www/materialcomponents/MaterialSpinner.java
+++ MaterialComponents/src/main/java/de/danihoo94/www/materialcomponents/MaterialSpinner.java
@@ -1,21 +1,27 @@
 package de.danihoo94.www.materialcomponents;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.text.Editable;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MaterialSpinner extends TextInputLayout {
-    private TextInputEditText editText;
+    private static final int ELLIPSIZE_END = 3;
+    private AutoCompleteTextView spinner;
+    private MaterialSpinnerAdapter adapter;
 
     /**
      * super class constructor with custom layout inflation
@@ -57,7 +63,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param type buffer type
      */
     public void setText(CharSequence text, TextView.BufferType type) {
-        editText.setText(text, type);
+        spinner.setText(text, type);
     }
 
     /**
@@ -67,7 +73,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param type  buffer type
      */
     public void setText(int resId, TextView.BufferType type) {
-        editText.setText(resId, type);
+        spinner.setText(resId, type);
     }
 
     /**
@@ -77,7 +83,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param resId   appearance resource id
      */
     public void setTextAppearance(Context context, int resId) {
-        editText.setTextAppearance(context, resId);
+        spinner.setTextAppearance(context, resId);
     }
 
     /**
@@ -87,7 +93,7 @@ public class MaterialSpinner extends TextInputLayout {
      */
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void setTextAppearance(int resId) {
-        editText.setTextAppearance(resId);
+        spinner.setTextAppearance(resId);
     }
 
     /**
@@ -99,20 +105,102 @@ public class MaterialSpinner extends TextInputLayout {
     private void setupView(@Nullable AttributeSet attrs, int defStyleAttr) {
 
         inflate(getContext(), R.layout.view_material_spinner, this);
-        editText = findViewById(R.id.view_material_edit_text);
+        spinner = findViewById(R.id.view_material_spinner_autocomplete);
+
+        spinner.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDropDown();
+            }
+        });
+
+        spinner.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                showDropDown();
+            }
+        });
+
 
         if (attrs != null) {
             TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.MaterialSpinner, defStyleAttr, 0);
 
             try {
-                //TODO
-                //String editTextHint = a.getString(R.styleable.MaterialEditTextView_editTextHint);
-                //editText.setHint(editTextHint);
+                ColorStateList textColor = a.getColorStateList(R.styleable.MaterialSpinner_android_textColor);
+                if (textColor != null) {
+                    spinner.setTextColor(textColor);
+                } else {
+                    spinner.setTextColor(ContextCompat.getColor(getContext(), android.R.color.black));
+                }
 
+                ColorStateList backgroundTint = a.getColorStateList(R.styleable.MaterialSpinner_android_backgroundTint);
+                if (textColor != null) {
+                    setBackgroundTintList(backgroundTint);
+                } else {
+                    setBackgroundColor(ContextCompat.getColor(getContext(), android.R.color.white));
+                }
+
+                boolean filled = a.getBoolean(R.styleable.MaterialSpinner_filled, false);
+                setFilled(filled);
             } finally {
                 a.recycle();
             }
         }
+    }
+
+    /**
+     * Sets if the spinner currently uses the filled theme (and not the outlined one)
+     *
+     * @param filled true, or, if outlined theme, false
+     */
+    public void setFilled(boolean filled) {
+        float paddingTop, paddingBottom;
+
+        if (filled) {
+            paddingTop = getResources().getDimension(R.dimen.material_spinner_padding_top_filled);
+            paddingBottom = getResources().getDimension(R.dimen.material_spinner_padding_bottom_filled);
+        } else {
+            paddingTop = getResources().getDimension(R.dimen.material_spinner_padding_top_outlined);
+            paddingBottom = getResources().getDimension(R.dimen.material_spinner_padding_bottom_outlined);
+        }
+
+        spinner.setPadding(spinner.getPaddingLeft(), (int) paddingTop, spinner.getPaddingRight(), (int) paddingBottom);
+    }
+
+    /**
+     * Set the maximum line count of the displayed element
+     *
+     * @param lines count of lines
+     */
+    public void setMaxLines(int lines) {
+        spinner.setMaxLines(lines);
+    }
+
+    /**
+     * Set the background color of the AutoCompleteTextView object
+     *
+     * @param color color as int
+     */
+    @Override
+    public void setBackgroundColor(int color) {
+        spinner.setBackgroundColor(color);
+    }
+
+    /**
+     * Set the background tint of the AutoCompleteTextView object
+     *
+     * @param tint color state list
+     */
+    @Override
+    public void setBackgroundTintList(@Nullable ColorStateList tint) {
+        spinner.setBackgroundTintList(tint);
+    }
+
+    /**
+     * Show dropdown menu
+     */
+    public void showDropDown() {
+        spinner.showDropDown();
     }
 
     /**
@@ -121,7 +209,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param errorText error text to be assigned, null to clear
      */
     public void setEditTextError(@Nullable CharSequence errorText) {
-        editText.setError(errorText);
+        spinner.setError(errorText);
     }
 
     /**
@@ -130,7 +218,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @return text
      */
     public Editable getText() {
-        return editText.getText();
+        return spinner.getText();
     }
 
     /**
@@ -139,7 +227,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param text text to be assigned
      */
     public void setText(CharSequence text) {
-        editText.setText(text);
+        spinner.setText(text);
     }
 
     /**
@@ -148,7 +236,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @param resId if od the string to be assigned
      */
     public void setText(int resId) {
-        editText.setText(resId);
+        spinner.setText(resId);
     }
 
     /**
@@ -157,7 +245,7 @@ public class MaterialSpinner extends TextInputLayout {
      * @return hint as text
      */
     public CharSequence getEditTextHint() {
-        return editText.getHint();
+        return spinner.getHint();
     }
 
     /**
@@ -166,7 +254,29 @@ public class MaterialSpinner extends TextInputLayout {
      * @param hintText hint text to be assigned, null to clear
      */
     public void setEditTextHint(@Nullable CharSequence hintText) {
-        editText.setHint(hintText);
+        spinner.setHint(hintText);
     }
 
+    /**
+     * Sets the adapter that stores the objects that should be displayed in the dropdown menu
+     *
+     * @param adapter The adapter to set
+     */
+    public void setAdapter(MaterialSpinnerAdapter adapter) {
+        this.adapter = adapter;
+        spinner.setAdapter(adapter);
+        adapter.setSpinner(spinner);
+    }
+
+    /**
+     * Sets the listener that will be notified when the user clicks an item in the drop down list
+     * and attaches the adapter to the listener, so that it stays informed about the current
+     * selection
+     *
+     * @param l the item click listener
+     */
+    public void setOnItemClickListener(@Nullable AdapterView.OnItemClickListener l) {
+        spinner.setOnItemClickListener(l);
+        adapter.attachToOnItemClickListener();
+    }
 }
--- SpinnerTest/src/main/java/de/danihoo94/www/spinnertest/TestActivity.java
+++ SpinnerTest/src/main/java/de/danihoo94/www/spinnertest/TestActivity.java
@@ -1,13 +1,33 @@
 package de.danihoo94.www.spinnertest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import de.danihoo94.www.materialcomponents.MaterialSpinner;
 
 public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        MaterialSpinner spinnerOutlined = findViewById(R.id.outlined);
+        TestAdapter adapter = new TestAdapter(this);
+        adapter.add(new Item("Test1"));
+        adapter.add(new Item("Test2"));
+        spinnerOutlined.setAdapter(adapter);
+
+        MaterialSpinner spinnerFilled = findViewById(R.id.filled);
+        spinnerFilled.setAdapter(adapter);
+
+        spinnerOutlined.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(adapter.getSelectedObject());
+            }
+        });
     }
 }
